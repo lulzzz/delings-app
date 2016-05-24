3daaebbd8fc9a57187570cfb49fa6f042e4c6242
vegsandv <vegard.sandvold@finn.no>
2016-05-24 22:51:31 +0200

    refactored store and thunk middleware

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@ import {
 
 import { Provider } from 'react-redux'
 
-import configureStore from './store/configureStore'
+import configureStore from './store'
 import App from './components/App'
 
 const store = configureStore()
